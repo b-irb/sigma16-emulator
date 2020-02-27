184970ad472c249ba8f151cc73edd4c63f3f9518
birb007 <42495242h@gmail.com>
2020-02-27 13:24:18 +0000

    add tests and main

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "vm.h"
+
+int main(int argc, char** argv)
+{
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s [filename]\n", argv[0]);
+        return -1;
+    }
+
+    char* fname = argv[1];
+    sigma16_vm_t* vm = NULL;
+
+    if (sigma16_vm_init(&vm, fname) < 0) {
+        perror("failed to initialise vm");
+        return -1;
+    }
+    if (sigma16_vm_exec(vm) < 0) {
+        perror("an error occured during execution");
+        goto error;
+    }
+    sigma16_vm_del(vm);
+    return 0;
+error:
+    sigma16_vm_del(vm);
+    return -1;
+}

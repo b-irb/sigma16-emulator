ef45ed228f6871f1b867fe409fae98b0f85fb868
birb007 <25516647+birb007@users.noreply.github.com>
2020-03-19 14:27:00 +0000

    reorganise project structure

---
+++ src/setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup, Extension
+setup(name="sigma16", version="1.0",
+        ext_modules=[Extension("sigma16", ["emulator/sigma16module.c", "emulator/tracing.c", "emulator/vm.c"],
+            extra_compile_args = ["-O2", "-DENABLE_TRACE", "-flto"])])
--- src/tracing.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "instructions.h"
-#include "vm.h"
-
-/* user defined trace handler */
-extern void sigma16_trace(sigma16_vm_t*, enum sigma16_instruction_fmt);

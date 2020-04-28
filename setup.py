f8bdd6110a858e2a0b144a153131d84ac9295d50
birb007 <25516647+birb007@users.noreply.github.com>
2020-04-28 16:55:53 +0100

    reorganisation

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup, Extension
 
+sigma16 = Extension(
+    "sigma16",
+    ["src/sigma16module.c", "src/tracing.c", "src/vm.c"],
+    extra_compile_args=["-O2", "-DPYTHON_COMPAT", "-flto"],
+)
+
 setup(
     name="sigma16",
     version="1.0",
-    ext_modules=[
-        Extension(
-            "sigma16",
-            ["src/sigma16module.c", "src/tracing.c", "src/vm.c"],
-            extra_compile_args=["-O2", "-DPYTHON_COMPAT", "-flto"],
-        )
-    ],
+    ext_modules=[sigma16],
+    description="Python bindings for Sigma16 emulator.",
 )

02b96c57e88b934190a05924e7168e898d118d84
birb007 <25516647+birb007@users.noreply.github.com>
2020-05-01 18:39:54 +0100

    only enable debugger

--- src/config.h
+++ src/config.h
@@ -5,12 +5,18 @@
 #define ENABLE_TRACE
 
 /* Enable post execution CPU dump*/
-#define ENABLE_CPU_DUMP
+/*
+ *#define ENABLE_CPU_DUMP
+ */
 
 /* Enable post execution memory dump */
-#define ENABLE_DUMP_MEM
+/*
+ *#define ENABLE_DUMP_MEM
+ */
 /* Specify memory dump limit (measured in bytes) */
-#define DUMP_MEM_LIM 0x200 >> 1
+/*
+ *#define DUMP_MEM_LIM 0x200 >> 1
+ */
 
 /* Constraints */
 #if defined(ENABLE_DEBUGGER) && !defined(ENABLE_TRACE)
